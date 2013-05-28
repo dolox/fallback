a2d02c24fa9c2f76a7ef8e68e10e1ed2d1516c50
Salvatore Garbesi <sal@garbesi.com>
2013-05-27 20:02:14 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 fallback.js
 ===========
 
-JavaScript library for dynamically loading javascript files from a CDN with a fallback.
+
+##About
+* _JavaScript library for dynamically loading javascript files from a CDN with a fallback option._
+* _Load scripts on demand for faster page load time!_
+* _Loads all scripts in parralel!_
+* __This is the only external script that needs to be loaded in your HTML!__
+
+
+# API
+##How to use it.
+### fallback.load(libraries)
+- Libaries is an object that expected it's key to be the libary that is loaded. Per example jQuery's key is `jQuery`.
+- You can pass either an array or string as the value 
+- All of the top level scripts will be executing in parallel.
+- The fallback scripts will be executed in the order they are in the array
+
+
+```
+<script src="fallback.min.js"></script>
+<script>
+	fallback.load({
+		jquery: [
+			'//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.FAIL_ON_PURPOSE.min.js',
+			'//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js',
+			'//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js'
+		],
+
+		stacktrace: '//cdnjs.cloudflare.com/ajax/libs/stacktrace.js/0.5.0/stacktrace.min.js'
+	}, function(success, failed) {
+		console.log('success');
+		console.log('-------');
+		console.log(success);
+
+		console.log('failed');
+		console.log('-------');
+		console.log(failed);
+	});
+</script>
+```
+
+
+##Changelog
+###0.01 / 2013-05-27
+	- Initial release.
---
+++ fallback.js
@@ -0,0 +1,87 @@
+fallback = {
+	callback: null,
+	head: document.getElementsByTagName('head')[0],
+
+	libraries: {},
+	libraries_count: 0,
+
+	loaded: {},
+	loaded_count: 0,
+
+	broken: {},
+	broken_count: 0
+};
+
+fallback.load = function(libraries, callback) {
+	var type = {};
+
+	if (!callback || (callback && type.toString.call(callback) !== '[object Function]')) {
+		callback = function() {};
+	}
+
+	this.callback = callback;
+	this.libraries = libraries;
+	this.initialize();
+};
+
+fallback.initialize = function() {
+	var library, url, urls;
+
+	for (library in this.libraries) {
+		urls = this.libraries[library];
+
+		if (!(urls instanceof Array)) {
+			this.libraries[library] = urls = [urls];
+		}
+
+		this.libraries_count++;
+		this.spawn(library, urls[0], 0);
+	}
+};
+
+fallback.error = function(library, index) {
+	index = parseInt(index);
+
+	if (!this.broken[library]) {
+		this.broken[library] = [];
+	}
+
+	this.broken[library][this.broken[library].length] = this.libraries[library][index];
+
+	if (index < this.libraries[library].length - 1) {
+		this.spawn(library, this.libraries[library][index + 1], index + 1);
+	} else {
+		this.broken_count++;
+	}
+
+	this.completed();
+};
+
+fallback.success = function(library, index) {
+	this.loaded[library] = this.libraries[library][index];
+	this.loaded_count++;
+	this.completed();
+};
+
+fallback.completed = function() {
+	if (this.libraries_count == this.loaded_count + this.broken_count) {
+		this.callback(this.loaded, this.broken);
+	}
+};
+
+fallback.spawn = function(library, url, index) {
+	var script = document.createElement('script');
+
+	script.src = url;
+	script.onload = function() {
+		fallback.success(library, index);
+	};
+
+	script.onerror = function() {
+		fallback.error(library, index);
+	};
+
+	this.head.appendChild(script);
+};
+
+window.fallback = fallback;
