63ad2c38a86ef18b7eb247103627d7b94e14be69
Salvatore Garbesi <sal@dolox.com>
2014-11-25 09:31:44 -0500

    Code quality adjustments

--- script/grunt/wrap.js
+++ script/grunt/wrap.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt, tasks) {
 	var me = {};
-	var files = {};
 	var uri = grunt.app.config.uri;
 
 	var packageJson = grunt.file.readJSON(grunt.app.config.uri.root + 'package.json');
@@ -9,27 +8,25 @@ module.exports = function(grunt, tasks) {
 	var wrapSuffixPrefix = 'me.banner = \'';
 	var wrapSuffixSuffix = '\';\n\nme.homepage = \'' + packageJson.homepage + '\';\n\nme.version = \'' + packageJson.version + '\';\n\nme.init();\n\n})(this);';
 
-	files.js = {};
-	files.js.dest = uri.dist + 'fallback.js';
-	files.js.options.wrapper = [wrapPrefix, wrapSuffixPrefix + packageJson.banner + wrapSuffixSuffix];
-	files.js.options = {};
-	files.js.src = [uri.dist + 'fallback.js'];
+	me.js = {};
+	me.js.dest = uri.dist + 'fallback.js';
+	me.js.options = {};
+	me.js.options.wrapper = [wrapPrefix, wrapSuffixPrefix + packageJson.banner + wrapSuffixSuffix];
+	me.js.src = [uri.distTmp + 'fallback.js'];
 
-	files.jsMin = {};
-	files.jsMin.dest = uri.distTmp + 'fallback.min.js';
-	files.jsMin.options.wrapper = [wrapPrefix, wrapSuffixPrefix + packageJson.name + wrapSuffixSuffix];
-	files.jsMin.options = {};
-	files.jsMin.src = [uri.distTmp + 'fallback.min.js'];
+	me.jsMin = {};
+	me.jsMin.dest = uri.distTmp + 'fallback.min.js';
+	me.jsMin.options = {};
+	me.jsMin.options.wrapper = [wrapPrefix, wrapSuffixPrefix + packageJson.name + wrapSuffixSuffix];
+	me.jsMin.src = [uri.distTmp + 'fallback.min.js'];
 
-	files.jsMinPost = {};
-	files.jsMinPost.dest = uri.dist + 'fallback.min.js';
-	files.jsMinPost.options.wrapper = [wrapBanner, ''];
-	files.jsMinPost.options = {};
-	files.jsMinPost.src = [uri.dist + 'fallback.min.js'];
+	me.jsMinPost = {};
+	me.jsMinPost.dest = uri.dist + 'fallback.min.js';
+	me.jsMinPost.options = {};
+	me.jsMinPost.options.wrapper = [wrapBanner, ''];
+	me.jsMinPost.src = [uri.dist + 'fallback.min.js'];
 
 	tasks.wrap = me;
-
-	//grunt.app.boot.gruntTasks('wrap/', tasks);
 
 	return tasks;
 };
